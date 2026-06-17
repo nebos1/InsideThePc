bb9ec6ff7d92548075e0ec7e76cb75338fc2d6c5
Nikolay Todorov <niki.htodorov@gmail.com>
2026-06-18 01:12:44 +0300

    scene2 params and logic

---
+++ src/scenes/scene2/scene2.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+import { GLTFLoader as GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const cpuUrl = new URL('../../assets/3Dmodels/CPU.glb', import.meta.url).href;
+
+export function buildScene2(group) {
+    // light + shadow
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+    const dirLight = new THREE.DirectionalLight(0xffffff, 10);
+    dirLight.position.set(-3, 4, -20);
+    dirLight.castShadow = true;
+    group.add(hemiLight);
+    group.add(dirLight);
+
+// model
+    const CPU = new THREE.Object3D();
+    const loader = new GLTFLoader();
+
+    loader.load(cpuUrl, (gltf) => {
+        CPU.add(gltf.scene);
+        group.add(CPU);
+
+        CPU.scale.set(1.6, 1.6, 1.6);
+        CPU.position.set(-2.25, 0, 0);
+        CPU.rotateX(0.3);
+        CPU.rotateY(2.2);
+        CPU.rotateZ(0.9);
+
+        CPU.traverse(object => {
+            if (object.isMesh) {
+                const oldMaterial = object.material;   
+                object.material = new THREE.MeshBasicMaterial({
+                    map: oldMaterial.map,
+                    color: oldMaterial.color,
+                    transparent: oldMaterial.transparent,
+                    opacity: oldMaterial.opacity,
+                });
+            }
+        });
+    });
+
+    return { CPU };
+}
---
+++ src/scenes/scene2/scene2.jsx
@@ -0,0 +1,17 @@
+import './scene2.css';
+
+function scene2() {
+    return (
+        <>              
+            <section id="scene-2" className="scene scene-2">
+                <div className="scene-content">
+                    <p id="scene-2-text"> Unleash the power of AI innovations in the CPU&apos;s to the edge. The heart and the power of fast processing instruction. Feel the power of high-speed, multithreading, gaming and multitasking. Experience the future of computing with cutting-edge CPU technology. </p>
+                </div>
+
+                <div className="scene-2-scroll-anchor" aria-hidden="true" />
+            </section>
+        </>    
+    );
+}
+
+export default scene2;
